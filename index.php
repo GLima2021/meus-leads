fff099d2c9a2fff55cfb236cc78f16a21f523e48
Gabriel Lima <gabriellima.sud@gmail.com>
2025-04-03 16:30:57 -0300

    Primeiro commit - adicionando arquivos do projeto

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+// Definir os números e suas respectivas probabilidades (pesos)
+$numeros = [
+    "5583988619696" => 4,  // Cainã (50%)
+    "5581985307047" => 3,  // Gabriel (20%)
+    "5585991724788" => 2,  // Anderson (20%)
+    "5534999224730" => 1   // Tarles (10%)
+    "5581985307047" => 3,  // Gabriel (20%)
+    "5583988619696" => 4,  // Cainã (50%)
+    "5585991724788" => 2,  // Anderson (20%)
+    "5534999224730" => 1   // Tarles (10%)
+];
+
+// Criar uma lista ponderada para sorteio justo
+$listaPonderada = [];
+foreach ($numeros as $numero => $peso) {
+    for ($i = 0; $i < $peso; $i++) {
+        $listaPonderada[] = $numero;
+    }
+}
+
+// Embaralhar a lista para evitar padrões previsíveis
+shuffle($listaPonderada);
+
+// Controle de sessão para distribuir os leads ciclicamente
+if (!isset($_SESSION['contador'])) {
+    $_SESSION['contador'] = 0;
+} else {
+    $_SESSION['contador'] = ($_SESSION['contador'] + 1) % count($listaPonderada);
+}
+
+// Escolher o número atual
+$numeroEscolhido = $listaPonderada[$_SESSION['contador']];
+
+// Mensagem que será enviada
+$mensagem = urlencode("Fala Tayan, quero mais informações sobre o Minicurso 💰!");
+
+// Criar o link do WhatsApp
+$url = "https://api.whatsapp.com/send?phone=$numeroEscolhido&text=$mensagem";
+
+// Redirecionar para o link do WhatsApp
+header("Location: $url");
+exit;
+?>
